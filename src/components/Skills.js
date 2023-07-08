e3a2f252f4d033d03aca807b2975268932566b49
ItsFaizan <faizanjavid71@gmail.com>
2023-07-08 17:12:50 +0500

    Skills added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,68 @@
 import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import Javascript from '../assets/javascript.png'
+import Mongo from '../assets/mongo.png'
+import react from '../assets/react.png'
+import Node from '../assets/node.png'
+import Github from '../assets/github.png'
+import Tailwind from '../assets/tailwind.png'
+
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name="skills" className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+          <p className='py-4'>These are the technologies I 've worked with</p>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py-8'>
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML} alt='html icon' />
+            <p className='my-4'>HTML</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt='html icon' />
+            <p className='my-4'>CSS</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Javascript} alt='html icon' />
+            <p className='my-4'>Javascript</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={react} alt='html icon' />
+            <p className='my-4'>React</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Node} alt='html icon' />
+            <p className='my-4'>Node JS</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Mongo} alt='html icon' />
+            <p className='my-4'>Mongo DB</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt='html icon' />
+            <p className='my-4'>Tailwind</p>
+           </div>
+
+           <div className=' hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Github} alt='html icon' />
+            <p className='my-4'>Github</p>
+           </div>
+
+
+        </div>
+      </div>
+
+    </div>
   )
 }
